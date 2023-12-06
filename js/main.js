856980cd542f514331ea11e6ba9c9d39d69f4e37
どらごん☆ぽーらん！ <145251687+DragonPolan@users.noreply.github.com>
2023-12-06 16:33:06 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const includeHeader = new XMLHttpRequest();
-includeHeader.open("GET", "https://dragonpolan.github.io/about/include/header.html", true);
+includeHeader.open("GET", "about/include/header.html", true);
 includeHeader.onreadystatechange = function () {
   if (includeHeader.readyState === 4 && includeHeader.status === 200) {
     const headerHTML = includeHeader.responseText;
